9d959988593871e2ffb861c5298e79f6ce83621a
Ryan <42234515+ollefrost@users.noreply.github.com>
2020-05-26 17:19:31 +0800

    Create haus.js

---
+++ haus.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $.getJSON("https://api.mcsrvstat.us/2/play.boredindahouse.ga", function(json) {
+        $("#desc").text(json.motd.clean[0]);
+        $("#version").text(json.version);
+        if (json.online === true) {
+            $("#status").text("Online ✔️");
+        } else {
+            $("#status").text("Offline ❌");
+        }
+        $("#players").text(json.players.online + "/" + json.players.max);
+        var names = "";
+        if (json.players.online > 0) {
+            for (var i = 0; i < json.players.list.length; i++) {
+                names += json.players.list[i];
+                if (i != json.players.list.length - 1) {
+                    names += ", ";
+                }
+            }
+        } else {
+            names += "Nobody is online ಠ_ಠ";
+        }
+        $("#names").text(names);
+    });
+
+});
