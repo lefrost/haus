c28cc8370c9f7194d4e529f0e76016ebd5eecd82
Ryan <42234515+ollefrost@users.noreply.github.com>
2020-05-27 14:40:10 +0800

    Update haus.js

--- haus.js
+++ haus.js
@@ -1,26 +1,31 @@
 $(document).ready(function () {
 
     $.getJSON("https://api.mcsrvstat.us/2/play.boredindahouse.ga", function(json) {
-        $("#desc").text("Asian Minecraft server :: " + json.motd.clean[0]);
-        $("#version").text(json.version);
         if (json.online === true) {
-            $("#status").text("Online ✔️");
+            $("#desc").text("Asian Minecraft server :: " + json.motd.clean[0]);
+			$("#version").text(json.version);
+			$("#status").text("Online ✔️");
+			$("#players").text(json.players.online + "/" + json.players.max);
+			var names = "";
+			if (json.players.online > 0) {
+				for (var i = 0; i < json.players.list.length; i++) {
+					names += json.players.list[i];
+					if (i != json.players.list.length - 1) {
+						names += ", ";
+					}
+				}
+			} else {
+				names += "Nobody is online ಠ_ಠ";
+			}
+			$("#names").text(names);
         } else {
+			$("#desc").text("N/A");
+			$("#version").text("N/A");
             $("#status").text("Offline ❌");
+			$("#players").text("N/A");
+			$("#names").text("Nobody is online ಠ_ಠ");
         }
-        $("#players").text(json.players.online + "/" + json.players.max);
-        var names = "";
-        if (json.players.online > 0) {
-            for (var i = 0; i < json.players.list.length; i++) {
-                names += json.players.list[i];
-                if (i != json.players.list.length - 1) {
-                    names += ", ";
-                }
-            }
-        } else {
-            names += "Nobody is online ಠ_ಠ";
-        }
-        $("#names").text(names);
+        
     });
     
     $("#creditsTag").hover(function() {
